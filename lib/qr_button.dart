f9599b2c704218301ab89a044e88e7cad70780e8
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-11-12 15:25:31 +0545

    add IntrinsicHeight for dividing icons

--- lib/qr_button.dart
+++ lib/qr_button.dart
@@ -10,23 +10,56 @@ void openQRDialog(BuildContext context) {
               borderRadius: BorderRadius.all(Radius.circular(8))),
           title: const Text(
             'What will you be parking today?',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
           ),
           actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.bike_scooter,
-                  color: Colors.black,
-                  size: 44,
-                )),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.car_rental,
-                  color: Colors.black,
-                  size: 44,
-                ))
+            // IconButton(
+            //    onPressed: () {},
+            //    padding: const EdgeInsets.all(33),
+            //    icon: const Icon(
+            //      Icons.two_wheeler,
+            //      color: Colors.black,
+            //         size: 50,
+            //    )),
+
+            //const IntrinsicHeight(child: VerticalDivider(color: Colors.black,thickness: 10,)),
+            IntrinsicHeight(
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      padding: const EdgeInsets.all(40),
+                      icon: const Icon(
+                        Icons.two_wheeler,
+                        color: Colors.black,
+                        size: 50,
+                      )),
+                  const VerticalDivider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    padding: const EdgeInsets.all(40),
+                    icon: const Icon(
+                      Icons.car_repair,
+                      color: Colors.black,
+                      size: 50,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //IconButton(
+            //  onPressed: () {},
+            //  padding: const EdgeInsets.all(33),
+            //  icon: const Icon(
+            //     Icons.car_repair,
+            //     color: Colors.black,
+            //      size: 50,
+            //    ),
+            //    ),
           ],
         );
       });
