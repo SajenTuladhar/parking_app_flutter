962c05408de46b7297255972e040b3a79e486e58
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-11-14 14:39:26 +0545

    add qr scanning page layout

--- lib/qr_scanner.dart
+++ lib/qr_scanner.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
 class QrScanner extends StatelessWidget {
-  const QrScanner
-({super.key});
+  const QrScanner({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('QRScanner'),),
+      appBar: AppBar(),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      ' Scan the QR code ',
+                      style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                )),
+            Expanded(
+                flex: 4,
+                child: Container(
+                  color: Colors.deepOrange,
+                )),
+            Expanded(
+                child: Container(
+              color: Colors.pink,
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
